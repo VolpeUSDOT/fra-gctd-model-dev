f8c4e8af8cbd7f29ab59753b70b4480502f9027c
Robert Rittmuller <robert.rittmuller@dot.gov>
2021-08-06 18:00:09 -0400

    Updated scene classifier training process

---
+++ PhaseTwo/scene_training_utils.py
@@ -0,0 +1,190 @@
+# common functions for simple CNN training
+from torch.utils.data import Subset, dataset
+from sklearn.model_selection import train_test_split
+import time
+import copy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+def get_class_distribution(dataset_obj):
+    idx2class = {v: k for k, v in dataset_obj.class_to_idx.items()}
+    count_dict = {k:0 for k,v in dataset_obj.class_to_idx.items()}
+    
+    for element in dataset_obj:
+        y_lbl = element[1]
+        y_lbl = idx2class[y_lbl]
+        count_dict[y_lbl] += 1
+            
+    return count_dict
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+def train_val_dataset(dataset, val_split=0.25):
+    train_idx, val_idx = train_test_split(list(range(len(dataset))), test_size=val_split)
+    datasets = {}
+    datasets['train'] = Subset(dataset, train_idx)
+    datasets['val'] = Subset(dataset, val_idx)
+    return datasets
+
+def save_labels(labels, label_file):
+    idx = 0
+    outputfile = open(label_file, "w")
+    for label in labels:
+        outputfile.write(str(idx) + ':' + str(label) + '\n')
+        idx += 1
+    
+    outputfile.close()
+
+def train_model(model, dataloaders, criterion, optimizer, device, num_classes, num_epochs=25, is_inception=False):
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  
+            else:
+                model.eval()   
+
+            running_loss = 0.0
+            running_corrects = 0
+            running_label_corrects = [0 for i in range(num_classes)]
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+
+                    if is_inception and phase == 'train':
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4*loss2
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+                for labelID in range(0, num_classes):
+                    # print("Preds[",labelID,"]", torch.where(preds == labelID, preds, -1))
+                    preds_bylabel = torch.where(preds == labelID, preds, -1)
+                    # print("Labels",labelID,"]", torch.where(labels.data == labelID, labels.data, -1))
+                    labels_bylabel = torch.where(labels.data == labelID, labels.data, -1)
+                    running_label_corrects[labelID] += torch.sum(preds_bylabel == labels_bylabel)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            # by label accuracy / precision / recall
+            for classID in range(0, num_classes):
+                classCount = 0
+                
+
+                # print("ClassID #", classID, " Acc:", running_label_corrects[classID].double() / classCount)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history, best_acc
+
+def initialize_model(models, model_name, num_classes, feature_extract, use_pretrained=True):
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":
+        """ Resnet18
+        """
+        model_ft = models.resnet18(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "alexnet":
+        """ Alexnet
+        """
+        model_ft = models.alexnet(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "vgg":
+        """ VGG11_bn
+        """
+        model_ft = models.vgg11_bn(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "squeezenet":
+        """ Squeezenet
+        """
+        model_ft = models.squeezenet1_1(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+
+    elif model_name == "densenet":
+        """ Densenet
+        """
+        model_ft = models.densenet201(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes) 
+        input_size = 224
+
+    elif model_name == "inception":
+        """ Inception v3 
+        Be careful, expects (299,299) sized images and has auxiliary output
+        """
+        model_ft = models.inception_v3(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+
+        num_ftrs = model_ft.AuxLogits.fc.in_features
+        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs,num_classes)
+        input_size = 299
+
+    else:
+        print("Invalid model name, exiting...")
+        exit()
+    
+    return model_ft, input_size
---
+++ PhaseTwo/train_scene_classification_model.py
@@ -0,0 +1,154 @@
+##################################################
+## Script to train a computer vision network to perform scene classification. 
+## This script fine-tunes a pre-trained computer vision network
+##################################################
+## MIT License
+##################################################
+## Author: Robert Rittmuller
+## Copyright: Copyright 2021, Volpe National Transportation Systems Center
+## Credits: Robert Rittmuller
+## License: MIT
+## Version: 0.1.1
+## Maintainer: Robert Rittmuller
+## Email: robert.rittmuller@dot.gov
+## Status: Active Development
+##################################################
+from __future__ import print_function
+from __future__ import division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+from pathlib import Path
+from collections import Counter
+from torchvision.transforms.transforms import Resize
+from scene_training_utils import *
+
+# training parameters
+batch_size = 32
+num_epochs = 26
+learning_rate = 0.002
+learning_momentum = 0.9
+num_workers = 12
+feature_extract = True
+
+# Detect if we have a GPU available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+if __name__ == '__main__':
+
+    print("PyTorch Version: ",torch.__version__)
+    print("Torchvision Version: ",torchvision.__version__)
+    print("Device in use: ",device)
+
+    # NOTE: project directory structure is as follows:
+    # ./example_dataset/
+    #       train/
+    #       val/
+    # ./models
+    # ./report
+
+    # location of the training data
+    data_dir = "/mnt/ml_data/FRA/Phase2/TrainingData/"
+    dataset_name = "grade_v1"
+
+    # where to put any visualizations
+    report_path = Path("PhaseTwo/report")
+    if not os.path.isdir(report_path):
+        os.mkdir(report_path)
+
+    # Where to save the class labels
+    label_file = "PhaseTwo/models/labels.txt"
+
+    # where to put the trainied models
+    model_path = Path("PhaseTwo/models")
+    if not os.path.isdir(model_path):
+        os.mkdir(model_path)
+
+    # Types of models =  [resnet, alexnet, vgg, squeezenet, densenet, inception]
+    # model_names = ['squeezenet','densenet']
+    model_names = ['squeezenet']
+
+    results = []
+
+    for model_name in model_names:
+
+        print("Initializing Datasets and Dataloaders...")
+
+        model_ft, input_size = initialize_model(models, model_name, 1, feature_extract, use_pretrained=True)
+
+        data_transforms = {
+            'train': transforms.Compose([
+                transforms.Resize(input_size),
+                transforms.ColorJitter(brightness=.2, contrast=.2, saturation=.2, hue=.1),
+                transforms.RandomRotation(1),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+            'val': transforms.Compose([
+                transforms.Resize(input_size),
+                # transforms.CenterCrop(input_size),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+}
+
+        dataset = datasets.ImageFolder(os.path.join(data_dir, dataset_name), transform=data_transforms['train'])
+        image_datasets = train_val_dataset(dataset)
+        dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=num_workers) for x in ['train', 'val']}
+        labels = image_datasets['train'].dataset.classes
+        num_classes = len(labels)
+
+        model_ft, input_size = initialize_model(models, model_name, num_classes, feature_extract, use_pretrained=True)
+        
+        model_ft = model_ft.to(device)
+
+        params_to_update = model_ft.parameters()
+        print('Number of classes:', num_classes)
+
+        class_counts = dict(Counter(sample_tup[1] for sample_tup in dataset))
+        class_counts = dict(sorted(class_counts.items()))
+        print(class_counts)
+
+        classNames = dataset.classes
+        print("Params to learn:")
+        if feature_extract:
+            params_to_update = []
+            for name,param in model_ft.named_parameters():
+                if param.requires_grad == True:
+                    params_to_update.append(param)
+                    print("\t",name)
+        else:
+            for name,param in model_ft.named_parameters():
+                if param.requires_grad == True:
+                    print("\t",name)
+
+        optimizer_ft = optim.SGD(params_to_update, lr=learning_rate, momentum=learning_momentum)
+
+        criterion = nn.CrossEntropyLoss()
+
+        model_ft, hist, best_acc = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft, device, num_classes, num_epochs=num_epochs, is_inception=(model_name=="inception"))
+
+        ohist = []
+
+        ohist = [h.cpu().numpy() for h in hist]
+
+        results.append(ohist)
+
+        plt.title("Validation Accuracy vs. Number of Training Epochs")
+        plt.xlabel("Training Epochs")
+        plt.ylabel("Validation Accuracy")
+        plt.plot(range(1,num_epochs+1),ohist,label='{:.0%}'.format(best_acc) + " Pretrained " + model_name)
+        plt.ylim((0,1.))
+        plt.xticks(np.arange(1, num_epochs+1, 0.5))
+        plt.legend()
+        plt.savefig(report_path / 'model-comparison.jpg')
+
+        save_labels(labels, label_file)
+        torch.save(model_ft, model_path / Path('saved_model_' + model_name + '.pt'))
